bc84b9b06f4fec88ecd11446b35df6ceb1bdd7dc
Long Qu <long.qu@wright.edu>
2015-06-07 10:27:18 -0400

    allowed MARGIN=0 for duplicated.matrix and anyDuplicated.matrix

--- R/duplicated.matrix.R
+++ R/duplicated.matrix.R
@@ -1,8 +1,20 @@
 duplicated.matrix = function (x, incomparables = FALSE, MARGIN = 1L, fromLast = FALSE, ...)
 {
-	if (!is.matrix(x) || !is.atomic(x) || !identical(incomparables, FALSE) || (MARGIN[1L]!=1L && MARGIN[1L]!=2L) || length(MARGIN)!=1L )
+	if (!is.matrix(x) || !is.atomic(x) || !identical(incomparables, FALSE) || ((nzeroMarg <-MARGIN[1L]!=0L) && MARGIN[1L]!=1L && MARGIN[1L]!=2L) || length(MARGIN)!=1L )
 		return(base::duplicated.matrix(x, incomparables, MARGIN, fromLast, ...))
-	.Call(C_dupAtomMat, x, as.integer(MARGIN), as.logical(fromLast))
+	if (nzeroMarg) {
+        .Call(C_dupAtomMat, x, as.integer(MARGIN), as.logical(fromLast))
+	}else{
+        att=attributes(x); dim(x)=c(as.integer(prod(att$dim)), 1L)
+        res=.Call(C_dupAtomMat, x, MARGIN=1L, as.logical(fromLast))
+        if(any(att$class=='factor')){
+            att$class= setdiff(att$class, c('ordered','factor'))
+            if(length(att$class)==0L) att$class=NULL
+            att$levels=NULL
+        }
+        attributes(res)=att
+        res
+	}
 }
 
 unique.matrix=function (x, incomparables = FALSE, MARGIN = 1, fromLast = FALSE, ...)
@@ -15,7 +27,12 @@ unique.matrix=function (x, incomparables = FALSE, MARGIN = 1, fromLast = FALSE, 
 
 anyDuplicated.matrix=function(x, incomparables = FALSE, MARGIN = 1, fromLast = FALSE, ...)
 {
-    if (!is.matrix(x) || !is.atomic(x) || !identical(incomparables, FALSE) || (MARGIN[1L]!=1L && MARGIN[1L]!=2L) || length(MARGIN)!=1L )
+    if (!is.matrix(x) || !is.atomic(x) || !identical(incomparables, FALSE) || ((nzeroMarg <-MARGIN[1L]!=0L) && MARGIN[1L]!=1L && MARGIN[1L]!=2L) || length(MARGIN)!=1L )
         return(base::anyDuplicated.matrix(x, incomparables, MARGIN, fromLast, ...))
-    .Call(C_anyDupAtomMat, x, as.integer(MARGIN), as.logical(fromLast))
+    if (nzeroMarg) {
+        .Call(C_anyDupAtomMat, x, as.integer(MARGIN), as.logical(fromLast))
+    }else{
+        dx=dim(x); dim(x)=c(as.integer(prod(dx)), 1L)
+        .Call(C_anyDupAtomMat, x, MARGIN=1L, as.logical(fromLast))
+    }
 }
--- tests/test.R
+++ tests/test.R
@@ -4,6 +4,7 @@ set.seed(9992722L, kind="Mersenne-Twister")
 x.double=model.matrix(~gl(5,8))[sample(40), ]
 x.integer=as.integer(x.double); attributes(x.integer)=attributes(x.double)
 x.factor=as.factor(x.integer); dim(x.factor)=dim(x.integer); dimnames(x.factor)=dimnames(x.integer)
+x.ordered=as.factor(x.integer); dim(x.ordered)=dim(x.integer); dimnames(x.ordered)=dimnames(x.integer)
 x.logical=as.logical(x.double); attributes(x.logical)=attributes(x.double)
 x.character=as.character(x.double); attributes(x.character)=attributes(x.double)
 x.complex=as.complex(x.double); attributes(x.complex)=attributes(x.double)
@@ -23,20 +24,24 @@ for(testi in 0:100){
     }
     xna.integer=as.integer(xna.double); attributes(xna.integer)=attributes(xna.double)
     xna.factor=as.factor(xna.integer); dim(xna.factor)=dim(xna.integer); dimnames(xna.factor)=dimnames(xna.integer)
+    xna.ordered=as.ordered(xna.integer); dim(xna.ordered)=dim(xna.integer); dimnames(xna.ordered)=dimnames(xna.integer)
     xna.logical=as.logical(xna.double); attributes(xna.logical)=attributes(xna.double)
     xna.character=as.character(xna.double); attributes(xna.character)=attributes(xna.double)
     xna.complex=as.complex(xna.double); attributes(xna.complex)=attributes(xna.double)
     xna.raw=suppressWarnings(as.raw(xna.double)); attributes(xna.raw)=attributes(xna.double)
     
-    x.objs = as.vector(outer(if(testi==0) c('x','xna') else 'xna', c('double','integer','factor', 'logical','character','complex','raw'),paste,sep='.'))
-    test.cases=expand.grid(x = x.objs, MARGIN=1:2, fromLast=c(FALSE, TRUE), stringsAsFactors=FALSE)
+    x.objs = as.vector(outer(if(testi==0) c('x','xna') else 'xna', c('double','integer','factor', 'ordered','logical','character','complex','raw'),paste,sep='.'))
+    test.cases=expand.grid(x = x.objs, MARGIN=0:2, fromLast=c(FALSE, TRUE), stringsAsFactors=FALSE)
     
     for(i in seq_len(nrow(test.cases))){
         this.case=as.list(test.cases[i,])
         this.case$x=get(this.case$x)
+        if(this.case$MARGIN != 0)
+            stopifnot(
+                identical(do.call(base::unique.matrix, this.case), 
+                          do.call(uniqueAtomMat::unique.matrix, this.case))
+            )
         stopifnot(
-            identical(do.call(base::unique.matrix, this.case), 
-                      do.call(uniqueAtomMat::unique.matrix, this.case)),
             identical(do.call(base::duplicated.matrix, this.case), 
                       do.call(uniqueAtomMat::duplicated.matrix, this.case)),
             identical(do.call(base::anyDuplicated.matrix, this.case), 
