78a33c8141509cc8e25c05fd54b8f3943cc8f130
Long Qu <long.qu@wright.edu>
2015-07-04 16:45:37 -0400

    added dbl_dig back into registration; cleanup all Makevars

--- src/init.c
+++ src/init.c
@@ -10,18 +10,19 @@
 	extern SEXP anyDupAtomMatHash(SEXP, SEXP, SEXP);
 	extern SEXP grpDupAtomMatHash(SEXP, SEXP, SEXP);
 	extern int initHash(void);
-	
 #else
 	#define HASHED_NAME(fun) fun
 	extern SEXP dupAtomMat(SEXP, SEXP, SEXP);
 	extern SEXP anyDupAtomMat(SEXP, SEXP, SEXP);
 	extern SEXP grpDupAtomMat(SEXP, SEXP, SEXP);
 #endif
+extern SEXP dbl_dig();
 
 static R_CallMethodDef callMethods[]  = {
   {"dupAtomMat", (DL_FUNC) &HASHED_NAME(dupAtomMat), 3},
   {"anyDupAtomMat", (DL_FUNC) &HASHED_NAME(anyDupAtomMat), 3},
   {"grpDupAtomMat", (DL_FUNC) &HASHED_NAME(grpDupAtomMat), 3},
+  {"dbl_dig", (DL_FUNC) &dbl_dig, 3},
   {NULL, NULL, 0}
 };
 	
